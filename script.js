b8e3b20902faae6f3dd4acaed58e9f3a46f28b8b
Akhilreddydevops <119477489+Akhilreddydevops@users.noreply.github.com>
2023-01-31 18:48:20 -0500

    updated css code & js code

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-const calculateBtn = document.querySelector("#calculateBtn");
-calculateBtn.addEventListener("click", function() {
-  const birthdate = new Date(document.querySelector("#birthdate").value);
-  const today = new Date();
-  const ageInMilliseconds = today - birthdate;
-  const ageInSeconds = ageInMilliseconds / 1000;
-  const ageInMinutes = ageInSeconds / 60;
-  const ageInHours = ageInMinutes / 60;
-  const ageInDays = ageInHours / 24;
-  const ageInWeeks = ageInDays / 7;
-  const ageInMonths = ageInDays / 30.44;
-  const ageInYears = ageInMonths / 12;
-  const leapYears = Math.floor(ageInYears / 4);
-
-  document.querySelector("#result").innerHTML = `
-    Age in Years: ${ageInYears.toFixed(2)}<br>
-    Age in Months: ${ageInMonths.toFixed(2)}<br>
-    Age in Weeks: ${ageInWeeks.toFixed(2)}<br>
-    Age in Days: ${ageInDays.toFixed(2)}<br>
-    Age in Hours: ${ageInHours.toFixed(2)}<br>
-    Age in Minutes: ${ageInMinutes.toFixed(2)}<br>
-    Age in Seconds: ${ageInSeconds.toFixed(2)}<br>
-    Leap Years: ${leapYears}
-  `;
-});
+const calculateAge = () => {
+    const birthdate = new Date(document.getElementById("birthdate").value);
+    const today = new Date();
+    const ageInMilliseconds = today - birthdate;
+    const ageInSeconds = ageInMilliseconds / 1000;
+    const ageInMinutes = ageInSeconds / 60;
+    const ageInHours = ageInMinutes / 60;
+    const ageInDays = ageInHours / 24;
+    const ageInWeeks = ageInDays / 7;
+    const ageInMonths = ageInDays / 30.44;
+    const ageInYears = ageInDays / 365.25;
+  
+    const result = document.getElementById("result");
+    result.innerHTML = `You are ${Math.floor(ageInYears)} years, ${Math.floor(
+      ageInMonths % 12
+    )} months, ${Math.floor(ageInWeeks % 4)} weeks, ${Math.floor(
+      ageInDays % 7
+    )} days, ${Math.floor(ageInHours % 24)} hours, ${Math.floor(
+      ageInMinutes % 60
+    )} minutes, and ${Math.floor(ageInSeconds % 60)} seconds old.`;
+    result.classList.add("show");
+  };
+  
+  document.getElementById("calculate").addEventListener("click", calculateAge);
+  
